a84f6831d6084df377db0f4e7db6252d8833633c
AlexCorleone <1034131730@qq.com>
2017-09-05 14:19:08 +0800

    Initial Commit

---
+++ CoreAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreAnimation
+//
+//  Created by AlexCorleone on 2017/9/5.
+//  Copyright © 2017年 魏乾坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
